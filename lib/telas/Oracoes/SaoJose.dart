3624c456bc57935571cf4fe2254022545c5e343a
Leandro <leandrotux@hotmail.com>
2021-02-15 09:22:25 -0300

    add orações

--- lib/telas/Oracoes/SaoJose.dart
+++ lib/telas/Oracoes/SaoJose.dart
@@ -11,35 +11,162 @@ class _SaoJoseState extends State<SaoJose> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //padding: EdgeInsets.all(10),
-        margin: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(children: [
-          Text(
-            "PAI NOSSO",
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.brown[700],
-              fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(children: [
+            Text(
+              "ORAÇÕES",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Text(
-            "Pai Nosso que estais nos Céus," +
-                "santificado seja o vosso Nome, " +
-                "venha a nós o vosso Reino, " +
-                "seja feita a vossa vontade " +
-                "assim na terra como no Céu. " +
-                "O pão nosso de cada dia nos dai hoje, " +
-                "perdoai-nos as nossas ofensas " +
-                "assim como nós perdoamos " +
-                "a quem nos tem ofendido, " +
-                "e não nos deixeis cair em tentação, " +
-                "mas livrai-nos do Mal.",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.brown[400],
+            Text(
+              "<<==========================>>",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ]),
+            Text(
+              "",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "PAI NOSSO",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Pai Nosso que estais nos Céus," +
+                  "santificado seja o vosso Nome, " +
+                  "venha a nós o vosso Reino, " +
+                  "seja feita a vossa vontade " +
+                  "assim na terra como no Céu. " +
+                  "O pão nosso de cada dia nos dai hoje, " +
+                  "perdoai-nos as nossas ofensas " +
+                  "assim como nós perdoamos " +
+                  "a quem nos tem ofendido, " +
+                  "e não nos deixeis cair em tentação, " +
+                  "mas livrai-nos do Mal.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.brown[400],
+              ),
+            ),
+            Text(
+              "<<==========================>>",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "AVÉ MARIA",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Avé Maria, cheia de graça, " +
+                  "o Senhor é convosco, " +
+                  "bendita sois vós entre as mulheres " +
+                  "e bendito é o fruto do vosso ventre, Jesus. " +
+                  "Santa Maria, Mãe de Deus, " +
+                  "rogai por nós pecadores, " +
+                  "agora e na hora da nossa morte. Ámen",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.brown[400],
+              ),
+            ),
+            Text(
+              "<<==========================>>",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "SANTO ANJO DO SENHOR",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Santo Anjo do Senhor, meu zeloso guardador, se a ti me confiou a piedade divina, sempre me rege, me guarda, me governa me ilumina. Amém",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.brown[400],
+              ),
+            ),
+            Text(
+              "<<==========================>>",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "À VOSSA PROTEÇÃO",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "À Vossa Proteção recorremos, Santa Mãe de Deus." +
+                  "Não desprezeis as nossas súplicas em nossas necessidades," +
+                  "mas livrai-nos sempre de todos os perigos," +
+                  "ó Virgem gloriosa e bendita.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.brown[400],
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
